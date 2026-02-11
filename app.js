e53aec6c6117d4ce1f2dbf713191284aeebcb74e
Nathan Rodrigues <w37975177@gmail.com>
2026-02-11 17:11:33 -0300

    Replace app.js with new progress bar implementation

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
-import { db } from "./firebase.js";
-import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+let meta = 3000;
+let arrecadado = 0;
 
-const docRef = doc(db, "metaRetiro", "dados");
+function atualizarBarra() {
+  const porcentagem = (arrecadado / meta) * 100;
+  document.getElementById("barraProgresso").style.width = porcentagem + "%";
+}
 
-getDoc(docRef).then((docSnap) => {
-  if (docSnap.exists()) {
-
-    const meta = docSnap.data().meta;
-    const atual = docSnap.data().atual;
-
-    document.getElementById("metaValor").innerText = meta;
-    document.getElementById("valorAtual").innerText = atual;
-
-    const porcentagem = (atual / meta) * 100;
-    document.getElementById("progress").style.width = porcentagem + "%";
-  }
-});
+atualizarBarra();
